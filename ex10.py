b14581c93f07d2e8fc5d5686f6c01cb72adbbc81
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-26 10:56:56 +0300

    what was that?

---
+++ ex10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backlash_cat)
+print(fat_cat)
